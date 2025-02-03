3cc6d445539fab48f6a86f991d602394ac2f8ac3
Chia <chiatung.yang@gmail.com>
2025-02-02 23:57:56 -0500

    init candy 135

---
+++ the_greedy_feast/candy.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+using std::vector;
+
+/**
+ * You are giving candies to these children subjected to the following requirements:
+ * 1. Each child must have at least one candy.
+ * 2. Children with a higher rating get more candies than their neighbors.
+ * 
+ * ratings = [1,0,2], returns 5 because 2,1,2
+ * ratings = [1,2,2], returns 4 because 1,2,1
+ */
+
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        //!TODO
+    }
+};
