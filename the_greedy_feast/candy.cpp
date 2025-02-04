55546f9d89cc7b0a67db0fbded64c4919e18cd95
Chia <chiatung.yang@gmail.com>
2025-02-03 22:08:27 -0500

    example with candy

--- the_greedy_feast/candy.cpp
+++ the_greedy_feast/candy.cpp
@@ -1,6 +1,9 @@
 #include <vector>
 using std::vector;
 
+#include <numeric>
+using std::accumulate, std::max;
+
 /**
  * You are giving candies to these children subjected to the following requirements:
  * 1. Each child must have at least one candy.
@@ -13,6 +16,25 @@ using std::vector;
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        //!TODO
+        // time: O(N), space: O(N)
+        // init all children with 1 cookie
+        // traverse from left to right 
+            // if left < right, right = left + 1
+        // traverse from right to left
+            // if right < left, left = max(right + 1, left)
+        int n = ratings.size();
+        vector<int> candies(n, 1);
+        for (int i = 1; i < n; ++i) {
+            if (ratings[i - 1] < ratings[i] ) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+        for (int i = n - 1; i > 0; --i) {
+            if (ratings[i] < ratings[i - 1]) {
+                candies[i - 1] = max(candies[i] + 1, candies[i - 1]);
+            }
+        }
+
+        return accumulate(candies.begin(), candies.end(), 0);
     }
-};
+}; // 135
