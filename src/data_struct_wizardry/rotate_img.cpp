1f768c04ea5c9b52791a56ff8b513c86cfa12473
Chia <chiatung.yang@gmail.com>
2025-02-19 20:44:11 -0500

    worked on rotating image

---
+++ src/data_struct_wizardry/rotate_img.cpp
@@ -0,0 +1,16 @@
+/**
+ * Input:
+    [[1,2,3],
+    [4,5,6],
+    [7,8,9]]
+ * Output:
+    [[7,4,1],
+    [8,5,2],
+    [9,6,3]]
+ */
+#include <vector>
+using std::vector;
+
+void rotate_img(vector<vector<int>>& img) {
+    
+} // 48
