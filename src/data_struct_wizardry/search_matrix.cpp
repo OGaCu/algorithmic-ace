4e8b746ad3fcdcd3bc37dec22dbe0e836c68fdc6
Chia <chiatung.yang@gmail.com>
2025-03-28 14:06:46 -0400

    search need opt

--- src/data_struct_wizardry/search_matrix.cpp
+++ src/data_struct_wizardry/search_matrix.cpp
@@ -6,3 +6,23 @@
  * 
  * You must write a solution in O(log(m * n)) time complexity.
  */
+
+#include <vector>
+using std::vector;
+
+// time complexity O(m + n)
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int m = matrix.size();
+    int n = matrix[0].size();
+    int i = 0, j = n - 1;
+    while (i < m && j >= 0) {
+        int val = matrix[i][j];
+        if (val == target) return true;
+        else if (val > target) j--;
+        else i++;
+    }
+    return false;
+} // 74
+
+// time complexity O(log(m+n)) 
+// use binary search; as a flattened 1d array
